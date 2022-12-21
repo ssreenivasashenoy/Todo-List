47e10fabead96579c827456ad113f1b4eecc2bcd
ssreenivasashenoy <sreenivasshenoy01@gmail.com>
2022-12-21 14:54:48 +0530

    Initial commit

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent {
+  title!:string;
+  desc!:string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  constructor(){}
+
+  ngOnInit():void {}
+  onSubmit(){
+    const todo = {
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+    this.todoAdd.emit(todo);
+  }
+}
---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit} from '@angular/core';
+import { Todo } from "../../Todo"
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+
+  todos: Todo[] = [];
+  localItem: string | null;
+  constructor(){ 
+    this.localItem = localStorage.getItem("todos");
+    
+    if(this.localItem==null){
+      this.todos=[];
+    }
+    else{
+      this.todos=JSON.parse(this.localItem);
+    }
+  }
+  ngOnInit(): void {
+
+  }
+  deleteTodo(todo:Todo) {
+    console.log(todo);
+    const i=this.todos.indexOf(todo);
+    this.todos.splice(i,1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  addTodo(todo:Todo) {
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+}
